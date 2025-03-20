a3e72deb3cd135e4db38be94f34b0899b1d59026
Maria Toledo <Nowaxial@users.noreply.github.com>
2025-03-20 11:37:05 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OddEven;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome! Let's see if your number is even or odd.");
+        Console.Write("Write a number: ");
+
+        string inputNumber = Console.ReadLine();
+
+        int numbers = int.Parse(inputNumber);
+
+
+        if (numbers % 2 == 0)
+        {
+            Console.WriteLine("Your number is even");
+        }
+        else
+        {
+            Console.WriteLine("Your number is odd");
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# OddEven
+ Tell if a number is odd or even.
